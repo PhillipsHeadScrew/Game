94e990083e729b856fc11974999ac335ea9e67f7
PhilipsHeadScrew <remus.drugan@icloud.com>
2023-01-14 21:09:14 +0100

    I added these

--- src/Chestplate.java
+++ src/Chestplate.java
@@ -1,2 +1,8 @@
-public class Chestplate {
+public class Chestplate extends Equipment{
+    int power;
+
+    Chestplate(int power){
+        this.type = ItemsType.CHESTPLATE;
+        this.power = power;
+    }
 }
