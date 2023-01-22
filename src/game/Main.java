12dfe0e181d44c6fbe5ddbc0d1b0335096a04c47
PhilipsHeadScrew <remus.drugan@icloud.com>
2023-01-22 14:09:18 +0100

    Added: screen system, printing screen.

--- src/game/Main.java
+++ src/game/Main.java
@@ -4,9 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         boolean running = true;
+        String equal = "==========================================";
+        String location = "no_mans_land";
+        int j = 0;
+        Screen screen = new Screen();
+
+        String[][] game_screen = screen.createScreen();
 
         while (running) {
-
+            switch (location) {
+                case "no_mans_land" -> j = 0;
+            }
+            System.out.println(equal);
+            for (int i = 0; i < 18; i++) {
+                System.out.println(" |" + game_screen[j][i] + "|");
+            }
+            System.out.println(equal);
         }
     }
 }
--- src/game/Screen.java
+++ src/game/Screen.java
@@ -2,5 +2,12 @@ package game;
 
 public class Screen {
 
+    public String[][] createScreen() {
+        String space = "                                      ";
 
+        String[][] Screen = {
+                {space, space, space, space, space, space, space, space, space, space, space, space, space, space, space, space, space, space}
+        };
+        return Screen;
+    }
 }
