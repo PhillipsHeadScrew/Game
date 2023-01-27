7f0e1a2b233b353271938b82f497ced1ab1561d3
PhilipsHeadScrew <remus.drugan@icloud.com>
2023-01-27 23:23:00 +0100

    Added: ability to access menu, exit menu to previous location

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +9,7 @@ public class Main {
         String equal = "==========================================";
         String location = "no_mans_land";
         int j = 0;
+        String previousLocation = "";
         Screen screen = new Screen();
 
         String[][] game_screen = screen.createScreen();
@@ -14,12 +17,32 @@ public class Main {
         while (running) {
             switch (location) {
                 case "no_mans_land" -> j = 0;
+                case "menu" -> j = 1;
             }
             System.out.println(equal);
             for (int i = 0; i < 18; i++) {
                 System.out.println(" |" + game_screen[j][i] + "|");
             }
             System.out.println(equal);
+
+            Scanner in = new Scanner(System.in);
+            String playerInput = in.next().toLowerCase() + in.nextLine();
+
+            switch (location) {
+                case "no_mans_land" -> {
+                    previousLocation = location;
+                    switch (playerInput) {
+                        case "menu" -> location = "menu";
+                    }
+                }
+                case "menu" -> {
+                    switch (playerInput) {
+                        case "back" -> location = previousLocation;
+                    }
+                }
+            }
+
+
         }
     }
 }
--- src/game/Screen.java
+++ src/game/Screen.java
@@ -3,10 +3,18 @@ package game;
 public class Screen {
 
     public String[][] createScreen() {
-        String space = "                                      ";
+        String space = "                                      ";//1
+        String lines = "     ––––––––––        ––––––––––     ";//3
+        String line = "     ––––––––––––––––––––––––––––     "; //4
+        String back = "     |           Back           |     "; //5
 
         String[][] Screen = {
-                {space, space, space, space, space, space, space, space, space, space, space, space, space, space, space, space, space, space}
+                {space, space, space, space, space, space, space, space, space, space, space, space, space, space,
+                        space, space, space, space},
+                {space, lines, "     | Pocket |        |Yourself|     ", lines, space, lines,
+                        "     |Notebook|        | Spells |     ", lines, space, lines,
+                        "     | Stuffs |        |KeyItems|     ", lines, space, space, line,
+                        back, line, space}
         };
         return Screen;
     }
